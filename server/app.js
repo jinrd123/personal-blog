46858856df14d067826ac638684616569095c5d0
靳荣达 <2368741842@qq.com>
2022-12-25 22:40:03 +0800

    服务端搭建

---
+++ server/app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const multer = require("multer");
+const app = express();
+const port = 8080;
+
+/*
+    配置跨域
+*/
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "content-type");
+    res.header("Access-Control-Allow-Methods", "DELETE,PUT,POST,GET,OPTIONS");
+    if (req.method == "OPTIONS") res.sendStatus(200); //让options尝试请求快速结束
+    else next();
+});
+
+/*
+    json参数解析
+*/
+app.use(express.json());
+
+/*
+    允许任何接口进行文件上传
+*/
+const update = multer({
+    dest: "./public/upload/temp"
+})
+app.use(update.any())
+
+app.get("/", (req, res) => {
+    res.send("hello world");
+})
+
+app.listen(port, () => {
+    console.log(`启动成功：http://localhost:${port}`);
+})
---
+++ server/note.md
@@ -0,0 +1,6 @@
+# 项目搭建
+
+1. `npm init -y`
+2. `npm i express multer splite3 uuid`
+
+3. `/utils/SnowFlake.js`：id生成工具（开源项目）
